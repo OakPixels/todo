3e2e5cb647916245d9e01bc772e5a60defa5a1e7
JoeCornish <g6doug@gmail.com>
2020-04-02 12:49:58 +0100

    Revert "updates"
    This reverts commit dfaff22344bc221e5944c6b487f2cc8a51d27e08.

--- static/my.js
+++ static/my.js
@@ -1,13 +1,11 @@
-var heading = document.getElementById("head")
-
 function resetTitle() {
-  heading.innerHTML = title;
+  let test = document.getElementById("head").innerHTML
+  if (test.slice(-4) != "List") {
+    document.getElementById("head").innerHTML = title;
+  };
 }
 
-let text = heading.innerHTML
-// If notification text then show title after 2 seconds
-if (text.slice(-4) != "List") {
-  setTimeout(function(){
-    resetTitle();
-  }, 2000);
-}
+// Reset title after 1 second
+setTimeout(function(){
+  resetTitle();
+}, 2000);
